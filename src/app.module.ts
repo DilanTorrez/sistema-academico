c128841478240debafe1cf40a7eb25107bf326d1
Dilan Torrez <dilantorrez601@gmail.com>
2023-05-26 17:56:45 -0400

    añadido swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//swagger 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { EstudiantesModule } from './modules/estudiantes/estudiantes.module';
 import { ProfesoresModule } from './modules/profesores/profesores.module';
 
@@ -21,3 +24,14 @@ import { ProfesoresModule } from './modules/profesores/profesores.module';
   providers: [AppService],
 })
 export class AppModule {}
+
+export function setupSwagger(app) {
+  const options = new DocumentBuilder()
+    .setTitle('Sistema Académico API')
+    .setDescription('API para el sistema académico')
+    .setVersion('1.0')
+    .addTag('sistema-academico')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
